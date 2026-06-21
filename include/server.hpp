7ccce8c1c42387480bfd330c57af44e47856acf8
Panish <panishdeveloper@gmail.com>
2026-06-21 15:34:21 +0300

    Add graceful shutdown

--- include/server.hpp
+++ include/server.hpp
@@ -1,21 +1,25 @@
 #pragma once
 
-#include <boost/asio/io_context.hpp>
 #include "thread_pool.hpp"
 #include "router.hpp"
 #include "logger.hpp"
 
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/signal_set.hpp>
+
 class HttpServer
 {
 public:
-    explicit HttpServer(unsigned short port, size_t numThreads = std::thread::hardware_concurrency(),
-                        std::unique_ptr<LogSink> sink = std::make_unique<ConsoleSink>());
+    explicit HttpServer(boost::asio::io_context& ioc, unsigned short port, size_t numThreads = std::thread::hardware_concurrency(),
+                                                      std::unique_ptr<LogSink> sink = std::make_unique<ConsoleSink>());
     void run();
 
 private:
+    boost::asio::io_context& m_ioc;
     unsigned short m_port;
-    boost::asio::io_context m_ioc;
     ThreadPool m_pool;
     Router m_router;
     AsyncLogger m_logger;
+    boost::asio::signal_set m_signals;
+    bool m_shutdownRequested = false;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ int main()
     unsigned int numThreads = std::thread::hardware_concurrency();
     if (numThreads == 0) numThreads = 4;
 
-    HttpServer server(8080, numThreads, std::make_unique<ConsoleSink>());
+    boost::asio::io_context ioc;
+    HttpServer server(ioc, 8080, numThreads, std::make_unique<ConsoleSink>());
     server.run();
 
     return 0;
