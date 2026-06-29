0f60bf8437a896e85b2b820c806731ebd4563a88
Panish <panishdeveloper@gmail.com>
2026-06-29 18:42:57 +0300

    Add global try/catch in main to log fatal exceptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
+#include <iostream>
+
 #include "server.hpp"
 
 int main(int argc, char* argv[])
 {
-    if (hasHelpFLag(argc, argv))
+    try
     {
-        printHelp();
+        if (hasHelpFLag(argc, argv))
+        {
+            printHelp();
+            return 0;
+        }
+
+        auto config = loadConfig(argc, argv);
+
+        boost::asio::io_context ioc;
+        HttpServer              server(ioc, config);
+        server.setLogLevel(config.log_level);
+        server.run();
+
         return 0;
     }
-
-    auto config = loadConfig(argc, argv);
-
-    boost::asio::io_context ioc;
-    HttpServer              server(ioc, config);
-    server.setLogLevel(config.log_level);
-    server.run();
-
-    return 0;
+    catch (const std::exception& e)
+    {
+        std::cerr << "[FATAL] Unhandled exception: " << e.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "[FATAL] Unknown exception" << std::endl;
+        return 1;
+    }
 }
